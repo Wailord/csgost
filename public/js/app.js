737dbdde3fb3656a7d9cfc445ea69322db9dc81f
Wailord <ryan.fox@oit.edu>
2015-08-15 16:30:36 -0700

    fixed bug in custom angular filter

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 angular.module('csghost', ['ngRoute', 'appRoutes', 'MainController', 'MatchService']).filter('reverse', function() {
   return function(items) {
+  	if(!items || !items.length) return;
     return items.slice().reverse();
   };
 });
