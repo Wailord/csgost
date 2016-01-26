53cf2f460ba53f896dcd7ab1e540c3e841a7d12e
Wailord <ryan.fox@oit.edu>
2016-01-26 00:57:24 -0800

    added a get request to generate match odds

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 var Match = require('./models/match');
 	MatchSummary = require('./models/match_summary');
+	Player = require('./models/player');
 var path = require('path');
 
 module.exports = function(app)
@@ -26,6 +27,73 @@ module.exports = function(app)
 			}
 		});
 	});
+
+	app.get('/api/players/:id', function(req, res) {
+		var id = req.params.id;
+		console.log('received a player query (GET player id ' + id + ')');
+
+		var acceptedParams = 0;
+
+		var query = Player.find();
+		query = query.find({'id': id})
+
+		query.exec(function (err, player)
+		{
+			if(err)
+				res.status(400).send(err);
+			else
+			{
+				res.setHeader('Content-Type','application/json');
+				res.send(player);
+			}
+		});
+	});
+
+	app.get('/api/odds', function(req, res) {
+		var t1 = req.query.t1.split(',');
+		var t2 = req.query.t2.split(',');
+
+		//console.log('received odds request:');
+		//console.log(t1);
+		//console.log(t2);
+
+		var bothTeams = t1.concat(t2);
+
+		var query = Player.find({id: {$in : bothTeams}});
+		query.exec(function (err, players)
+		{
+			if(err) {
+				console.log('error getting match odds: ' + err);
+				res.status(400).send(err);
+			}
+			else if(players.length != 10)
+			{
+				res.send('Could not find all players. (found ' + players.length + ')');
+			}
+			else
+			{
+				var t1AvgRating = 0;
+				var t2AvgRating = 0;
+
+				players.forEach(function(player)
+				{
+					if(t1.indexOf(String(player.id)) > -1)
+						t1AvgRating += player.rating;
+					if(t2.indexOf(String(player.id)) > -1)
+						t2AvgRating += player.rating;
+				});
+
+				t1AvgRating /= 5;
+				t2AvgRating /= 5;
+
+				var team1WinChance = ((t1AvgRating - t2AvgRating) / 850) + 0.5;
+
+				res.setHeader('Content-Type','application/json');
+				res.send('Team1 chance: ' + team1WinChance);
+			}
+		});
+	});
+
 
 	app.get('/api/matches/:id', function(req, res) 
 	{
